a784dbb5e2db5b62faebf6a0d861fd875af1ae63
lmontero18 <lmonterom879@gmail.com>
2025-01-31 21:46:11 -0600

    fix: typo

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,7 +1,7 @@
-function Title() {
+function title() {
   return (
     <h1 className="text-4xl font-bold text-center text-white">JS Quiz Game</h1>
   );
 }
 
-export default Title;
+export default title;

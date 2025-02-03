bc4e6326e9089fbef928dd004bfbd8d1a34bcee1
lmontero18 <lmonterom879@gmail.com>
2025-02-02 19:14:08 -0600

    fix: colors in title

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,8 +1,6 @@
 const Title = () => {
   return (
-    <h1 className="text-4xl font-bold text-center text-green-600">
-      JS Quiz Game
-    </h1>
+    <h1 className="text-4xl font-bold text-center text-white">JS Quiz Game</h1>
   );
 };
 
